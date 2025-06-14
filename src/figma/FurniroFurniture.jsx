ee0e1f15261a9378e00b91023f6b107c40aa976e
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-06-14 11:31:28 -0700

    complete figma

--- src/figma/FurniroFurniture.jsx
+++ src/figma/FurniroFurniture.jsx
@@ -1,6 +1,29 @@
+import "./Not.css";
+
 function FurniroFurniture(){
     return(
         <>
+        <div className=" FurniroFurniture">
+            <p>Share your setup with</p>
+            <h1>#FurniroFurniture</h1>
+            <div className="div1">
+                <img src="https://furniro.archisacademy.com/assets/sideStand-4c81503a.png"/>
+                                <img src="https://furniro.archisacademy.com/assets/guestBedroom-1b81f4f3.png"/>
+                <img src="https://furniro.archisacademy.com/assets/wallDecor-b7bdb306.png"/>
+                <img src="https://furniro.archisacademy.com/assets/kitchen-9150ef78.png"/>
+                <img src="https://furniro.archisacademy.com/assets/cornerStand-654257cc.png"/>
+            </div>
+                        <div className="div2">
+<img src="https://furniro.archisacademy.com/assets/sofabed-97fc7fe5.jpg"/>
+                   <img src="https://furniro.archisacademy.com/assets/recliner-b01255b8.jpg"/>
+<img src="https://furniro.archisacademy.com/assets/patioFurniture-ceb61298.jpg"/>
+<img src="https://furniro.archisacademy.com/assets/massageChair-8cf3b684.jpg"/>
+{/* <img src="https://furniro.archisacademy.com/assets/executiveDesk-dffabd18.jpg" /> */}
+
+        
+                        </div>
+
+        </div>
         </>
     )
 }
